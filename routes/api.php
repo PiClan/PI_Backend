defc003a856322970ce8f44e2eaf93edd40326df
jonatanhp <jonatan.hp@upeu.edu.pe>
2021-06-22 18:57:52 -0500

    alumnos_backend

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AlumnoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,6 @@ Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [RegisterController::class, 'login']);
      
 Route::middleware('auth:api')->group( function () {
-    
+    Route::resource('alumnos', AlumnoController::class);
     
 });
